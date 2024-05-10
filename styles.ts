a0b8ae2d20f8d1741e01557830fce1cb10d4d36f
ALINE APARECIDA ALVES <aline.aalves9@senacsp.edu.br>
2024-05-10 20:46:54 -0300

    my-app

---
+++ styles.ts
@@ -0,0 +1,31 @@
+import{ StyleSheet} from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      padding: 55,
+      //alignItems: 'center',
+      //justifyContent: 'center',
+    },
+    title:{
+        fontSize:36,
+        fontFamily:'bold',
+        lineHeight:44,
+        marginTop:42,
+
+    },
+    subtitle:{
+
+
+    },
+    message:{
+        fontSize:20,
+        color:'#7c7c8a',
+        marginTop:12,
+        marginBottom:38,
+
+    },
+
+  });
